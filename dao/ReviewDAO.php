9a97eb8213e913cfe4234e4000118fe97524aac0
Joao vitor <joaovitor12j@gmail.com>
2023-01-25 23:29:07 -0300

    Ajustes

--- dao/ReviewDAO.php
+++ dao/ReviewDAO.php
@@ -1,139 +1,139 @@
 <?php
 
-  include_once("models/Review.php");
-  include_once("models/Message.php");
+    include_once("models/Review.php");
+    include_once("models/Message.php");
 
-  // Necessário para resgatar dados do usuário
-  include_once("dao/userDAO.php");
+    // Necessário para resgatar dados do usuário
+    include_once("dao/userDAO.php");
 
-  class ReviewDAO implements REviewDAOInterface {
+    class ReviewDAO implements REviewDAOInterface {
 
     private $conn;
     private $url;
     private $message;
 
     public function __construct(PDO $conn, $url) {
-      $this->conn = $conn;
-      $this->url = $url;
-      $this->message = new Message($url);
+        $this->conn = $conn;
+        $this->url = $url;
+        $this->message = new Message($url);
     }
 
     public function buildReview($data) {
 
-      $reviewObject = new Review();
+        $reviewObject = new Review();
 
-      $reviewObject->id = $data["id"];
-      $reviewObject->rating = $data["rating"];
-      $reviewObject->review = $data["review"];
-      $reviewObject->user_id = $data["user_id"];
-      $reviewObject->movie_id = $data["movie_id"];
+        $reviewObject->id = $data["id"];
+        $reviewObject->rating = $data["rating"];
+        $reviewObject->review = $data["review"];
+        $reviewObject->user_id = $data["user_id"];
+        $reviewObject->movie_id = $data["movie_id"];
 
-      return $reviewObject;
+        return $reviewObject;
 
     }
 
     public function create(Review $review) {
 
-      $stmt = $this->conn->prepare("INSERT INTO review (
-        rating, review, user_id, movie_id
-      ) VALUES (
-        :rating, :review, :user_id, :movie_id
-      )");
+        $stmt = $this->conn->prepare("INSERT INTO review (
+            rating, review, user_id, movie_id
+        ) VALUES (
+            :rating, :review, :user_id, :movie_id
+        )");
 
-      $stmt->bindParam(":rating", $review->rating);
-      $stmt->bindParam(":review", $review->review);
-      $stmt->bindParam(":user_id", $review->user_id);
-      $stmt->bindParam(":movie_id", $review->movie_id);
+        $stmt->bindParam(":rating", $review->rating);
+        $stmt->bindParam(":review", $review->review);
+        $stmt->bindParam(":user_id", $review->user_id);
+        $stmt->bindParam(":movie_id", $review->movie_id);
 
-      $stmt->execute();
+        $stmt->execute();
 
-      // Redireciona e apresenta mensagem de sucesso
-      $this->message->setMessage("Comentário adicionado com sucesso!", "success", "index.php");
+        // Redireciona e apresenta mensagem de sucesso
+        $this->message->setMessage("Comentário adicionado com sucesso!", "success", "index.php");
 
     }
 
     
     public function getMovieReviews($id) {
 
-      $reviews = [];
+        $reviews = [];
 
-      // encontrar as reviews na tabela de review
-      $stmt = $this->conn->prepare("SELECT * FROM review WHERE movie_id = :id");
+        // encontrar as reviews na tabela de review
+        $stmt = $this->conn->prepare("SELECT * FROM review WHERE movie_id = :id");
 
-      $stmt->bindParam("id", $id);
+        $stmt->bindParam("id", $id);
 
-      $stmt->execute();
+        $stmt->execute();
 
-      if($stmt->rowCount() > 0) {
+        if($stmt->rowCount() > 0) {
 
-        $userDao = new userDAO($this->conn, $this->url);
+            $userDao = new userDAO($this->conn, $this->url);
 
-        $reviewsArray = $stmt->fetchAll();
+            $reviewsArray = $stmt->fetchAll();
 
-        foreach($reviewsArray as $review) {
+            foreach($reviewsArray as $review) {
 
-          $reviewObject = $this->buildReview($review);
+            $reviewObject = $this->buildReview($review);
 
-          // pegar dados do usuário
-          $user = $userDao->findById($reviewObject->user_id);
+            // pegar dados do usuário
+            $user = $userDao->findById($reviewObject->user_id);
 
-          // Adiciona usuário na review
-          $reviewObject->user = $user;
-          
-          $reviews[] = $reviewObject;
+            // Adiciona usuário na review
+            $reviewObject->user = $user;
+            
+            $reviews[] = $reviewObject;
 
+            }
+            
         }
-        
-      }
 
-      return $reviews;
+        return $reviews;
 
     }
 
     public function hasAlreadyReviewed($id, $userId) {
 
-      // verificar se usuário já fez review no filme
-      $stmt = $this->conn->prepare("SELECT * FROM review WHERE movie_id = :id AND user_id = :userid");
+        // verificar se usuário já fez review no filme
+        $stmt = $this->conn->prepare("SELECT * FROM review WHERE movie_id = :id AND user_id = :userid");
 
-      $stmt->bindParam(":id", $id);
-      $stmt->bindParam(":userid", $userId);
+        $stmt->bindParam(":id", $id);
+        $stmt->bindParam(":userid", $userId);
 
-      $stmt->execute();
+        $stmt->execute();
 
-      if($stmt->rowCount() > 0) {
+        if($stmt->rowCount() > 0) {
         return true;
-      } else {
+        } else {
         return false;
-      }
+        }
 
     }
 
     // Recebe todas as avaliações de um filme, e calcula a média
     public function getRatings($id) {
 
-      $stmt = $this->conn->prepare("SELECT * FROM review WHERE movie_id = :id");
+        $stmt = $this->conn->prepare("SELECT * FROM review WHERE movie_id = :id");
 
-      $stmt->bindParam(":id", $id);
+        $stmt->bindParam(":id", $id);
 
-      $stmt->execute();
+        $stmt->execute();
 
-      $rating = "Não avaliado";
+        $rating = "Não avaliado";
 
-      if($stmt->rowCount() > 0) {
+        if($stmt->rowCount() > 0) {
 
-        $rating = 0;
+            $rating = 0;
 
-        $reviews = $stmt->fetchAll();
+            $reviews = $stmt->fetchAll();
 
-        foreach($reviews as $review) {
-          $rating += $review["rating"];
+            foreach($reviews as $review) {
+            $rating += $review["rating"];
+            }
+
+            $rating = $rating / count($reviews);
+    
         }
 
-        $rating = $rating / count($reviews);
-  
-      }
-
-      return $rating;
+        return $rating;
 
     }
 
